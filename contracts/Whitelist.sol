85c40f914bdd09e0a498a7a09c32af4bc076ab9c
ToraMochii <donghyeok.dev@gmail.com>
2023-04-24 16:27:07 +0900

    whitelist deploy 작성중

---
+++ contracts/Whitelist.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.8.0;
+
+contract Whitelist{
+    // 화이트리스트 최대 허용 수
+    uint8 public maxWhitelistedAddresses;
+
+    // 허용된 화이트리스트를 확인, 모든 기본값은 default 가 false 임
+    mapping(address => bool) public whitelistedAddress;
+
+    // 허용된 화이트리스트가 몇명인지 확인한다.
+    uint8 public numAddressWhitelisted;
+
+    // 배포시 최대 화이트리스트 수를 설정한다.
+    constructor(uint8 _maxWhitelistedAddresses){
+        maxWhitelistedAddresses =  _maxWhitelistedAddresses;
+    }
+
+
+    function addAddressToWhitelist() public {
+        // 화이트리스트에 추가된 계정인지 확인
+        require(!whitelistedAddress[msg.sender], "이 주소는 이미 화이트리스트에 추가되었습니다.");
+        // 현재 추가된 화이트리스트가 최초로 설정한 화이트리스트 수를 넘었는지를 확인
+        require(numAddressWhitelisted < maxWhitelistedAddresses, "화이트 리스트를 더 추가 할 수 없습니다.");
+
+        // 두가지 조건을 만족하면 화이트리스트에 해당 주소를 추가한다.
+        whitelistedAddress[msg.sender] = true;
+        // 화이트리스트 카운트도 증가시킨다.
+        numAddressWhitelisted += 1;
+    }
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+const {ethers} = require("hardhat");
+
+async function main() {
+  const whitelistContract = await ethers.getContractFactory("Whitelist");
+  // 인스턴스 초기화시 받는 maxWhitelist 수
+  const deployedWhitelistContract = await whitelistContract.deploy(10);
+
+  // 배포가 완료될때까지 대기
+  await deployedWhitelistContract.deployed();
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
